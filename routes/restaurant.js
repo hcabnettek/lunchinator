05ba59a5ddd19dee9300bb9fc638223abb93d03c
Chris Kettenbach <kettenbach@gmail.com>
2012-10-08 11:17:39 -0700

    added local env file

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
   , url = require('url');
 
-var MONGOHQ_URL = process.env.MONGOHQ_URL || "mongodb://heroku:46deb6c002587ca15b736ef3bca3815f@alex.mongohq.com:10062/app8173016";
+var MONGOHQ_URL = process.env.MONGOHQ_URL || 'mongodb://heroku:46deb6c002587ca15b736ef3bca3815f@alex.mongohq.com:10062/app8173016';
 var db = mongoose.createConnection(MONGOHQ_URL);
 
 var restaurantSchema = new mongoose.Schema({
@@ -9,9 +9,7 @@ var restaurantSchema = new mongoose.Schema({
 	address: String
 });
 
-var Restaurant = mongoose.model('Restaurant', restaurantSchema, "Restaurant");
-
-
+var Restaurant = mongoose.model('Restaurant', restaurantSchema, 'Restaurant');
 
 exports.list = function(req, res){
   res.render('restaurant', { title: 'Restaurants' });
