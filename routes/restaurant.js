e9780dc55117dcb6df5d35cafd7cbfa17b859178
Chris Kettenbach <kettenbach@gmail.com>
2012-10-19 10:29:06 -0700

    testing session support

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,10 +2,10 @@ var db = require('mongoose'),
  Restaurant = require('../db').Restaurant;
 
 exports.list = function(req, res){
-  
+  req.session.foo = 'bar';
   Restaurant.find({}, function(err, restaurants){
   	console.log(restaurants);
-  	res.render('restaurant', { title: 'Restaurants', restaurants: restaurants });
+  	res.render('restaurant', { title: req.session.foo, restaurants: restaurants });
   });
   
 };
