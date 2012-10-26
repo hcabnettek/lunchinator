dc5051464dad93afdb027f4e455b8a263e8f8b07
Chris Kettenbach <kettenbach@gmail.com>
2012-10-26 13:05:43 -0700

    Added user checkin post functionality

--- db.js
+++ db.js
@@ -6,5 +6,11 @@ var restaurantSchema = new Schema({
 	Address: String
 });
 
+var userCheckInSchema = new Schema({
+	Name: String,
+	Email: String
+});
+
 mongoose.connect(process.env.MONGOHQ_URL);
 exports.Restaurant = mongoose.model('Restaurant', restaurantSchema, 'Restaurant');
+exports.UserCheckIn = mongoose.model('CheckedInUser', userCheckInSchema, 'CheckedInUser');
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
-
-/*
- * GET users listing.
- */
+var db = require('mongoose'),
+ Restaurant = require('../db').UserCheckIn;
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
 
 exports.checkin = function(req, res){
-  res.send("user checked in");
+ 	var checkinuser = new UserCheckIn({ Name: req.body.name, Address: req.body.email });
+	checkinuser.save(function (err) {
+	  	res.send("user check in");
+	});  
 };
